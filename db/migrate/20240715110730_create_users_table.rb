6722eda6ee4f53225df408bcdced8f14e09142d8
shino omata <shino410stx@gmail.com>
2024-07-15 20:10:25 +0900

    Modify migration to avoid creating existing users table

---
+++ db/migrate/20240715110730_create_users_table.rb
@@ -0,0 +1,5 @@
+class CreateUsersTable < ActiveRecord::Migration[7.0]
+  def change
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_14_110025) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_15_110730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
