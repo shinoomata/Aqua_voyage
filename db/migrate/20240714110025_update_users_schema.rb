e4fabe964b7bf85ea41ad3a43eae796c45326ae6
shino omata <shino410stx@gmail.com>
2024-07-15 20:14:31 +0900

    Check if users table exists before adding columns

--- db/migrate/20240714110025_update_users_schema.rb
+++ db/migrate/20240714110025_update_users_schema.rb
@@ -1,31 +1,33 @@
 class UpdateUsersSchema < ActiveRecord::Migration[7.0]
   def change
-    unless column_exists?(:users, :provider)
-      add_column :users, :provider, :string
-    end
+    if table_exists?(:users)
+      unless column_exists?(:users, :provider)
+        add_column :users, :provider, :string
+      end
 
-    unless column_exists?(:users, :uid)
-      add_column :users, :uid, :string
-    end
+      unless column_exists?(:users, :uid)
+        add_column :users, :uid, :string
+      end
 
-    unless column_exists?(:users, :username)
-      add_column :users, :username, :string
-    end
+      unless column_exists?(:users, :username)
+        add_column :users, :username, :string
+      end
 
-    unless column_exists?(:users, :name)
-      add_column :users, :name, :string
-    end
+      unless column_exists?(:users, :name)
+        add_column :users, :name, :string
+      end
 
-    unless column_exists?(:users, :oauth_token)
-      add_column :users, :oauth_token, :string
-    end
+      unless column_exists?(:users, :oauth_token)
+        add_column :users, :oauth_token, :string
+      end
 
-    unless column_exists?(:users, :oauth_expires_at)
-      add_column :users, :oauth_expires_at, :datetime
-    end
+      unless column_exists?(:users, :oauth_expires_at)
+        add_column :users, :oauth_expires_at, :datetime
+      end
 
-    unless column_exists?(:users, :image)
-      add_column :users, :image, :string
+      unless column_exists?(:users, :image)
+        add_column :users, :image, :string
+      end
     end
   end
 end
