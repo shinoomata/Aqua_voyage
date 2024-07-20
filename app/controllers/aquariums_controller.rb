27d86cf3c82dc29633d5ed3493a426033f69a6b1
shino omata <shino410stx@gmail.com>
2024-07-20 10:28:58 +0900

    地域をプルダウンで選択して検索できるようにした

--- app/controllers/aquariums_controller.rb
+++ app/controllers/aquariums_controller.rb
@@ -2,11 +2,16 @@ class AquariumsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @q = Aquarium.ransack(params[:q] )
+    @q = Aquarium.ransack(params[:q])
+    @regions = Aquarium.distinct.pluck(:region)
     @aquariums = @q.result(distinct: true)
   end
 
   def show
     @aquariums = Aquarium.find(params[:id])
+    unless @aquarium
+      flash[:alert] = "Aquarium not found"
+      redirect_to aquariums_path
+    end
   end
 end
