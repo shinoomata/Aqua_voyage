4dc04a321435f7a9fab47bef2809d8ea5622fe2b
shino omata <shino410stx@gmail.com>
2024-07-28 14:53:18 +0900

    グラフの色、位置変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,40 @@
 module ApplicationHelper
+  def daisyui_colors_1
+  [
+    "#3b82f6",  # primary
+    "#60a5fa",  # secondary
+    "#ec4899",  # accent
+    "#f87171",  # red
+    "#facc15",  # yellow
+    "#34d399",  # green
+    "#6b7280",  # gray
+    "#d1d5db"   # light-gray
+  ]
+  end
+
+  def daisyui_colors_2
+    [
+      "#4ade80",  # green
+      "#22d3ee",  # cyan
+      "#a78bfa",  # purple
+      "#fb923c",  # orange
+      "#f43f5e",  # rose
+      "#10b981",  # emerald
+      "#3b82f6",  # blue
+      "#d1d5db"   # light-gray
+    ]
+  end
+
+  def daisyui_colors_3
+    [
+      "#f97316",  # orange
+      "#10b981",  # green
+      "#8b5cf6",  # violet
+      "#f43f5e",  # rose
+      "#06b6d4",  # cyan
+      "#d946ef",  # pink
+      "#3b82f6",  # blue
+      "#d1d5db"   # light-gray
+    ]
+  end
 end
