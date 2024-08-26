4bbd16e8ef0ce67d21cbac730fb5ed19798a0cab
shino omata <shino410stx@gmail.com>
2024-08-26 21:58:33 +0900

    Configure Redis URL based on environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,11 @@
 # config/initializers/sidekiq.rb
 
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://redis:6379/0' }
+    redis_url = Rails.env.production? ? 'redis://aqua-voyage-redis.fly.dev:6379/0' : 'redis://localhost:6379/0'
+    config.redis = { url: redis_url }
   end
   
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://redis:6379/0' }
+    redis_url = Rails.env.production? ? 'redis://aqua-voyage-redis.fly.dev:6379/0' : 'redis://localhost:6379/0'
+    config.redis = { url: redis_url }
   end
-  
