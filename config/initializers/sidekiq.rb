b78f60c529465fc269b2d3ff92a8b673ada56cff
shino omata <shino410stx@gmail.com>
2024-08-28 21:35:57 +0900

    独自ドメイン設定

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,12 @@
 # config/initializers/sidekiq.rb
 
 Sidekiq.configure_server do |config|
-    redis_url = Rails.env.production? ? 'redis://aqua-voyage-redis.fly.dev:6379/0' : 'redis://localhost:6379/0'
+    redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/0'
     config.redis = { url: redis_url }
   end
   
   Sidekiq.configure_client do |config|
-    redis_url = Rails.env.production? ? 'redis://aqua-voyage-redis.fly.dev:6379/0' : 'redis://localhost:6379/0'
+    redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/0'
     config.redis = { url: redis_url }
   end
+  
