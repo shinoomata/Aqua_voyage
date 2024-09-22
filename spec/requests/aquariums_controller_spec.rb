6a834517032a521b028a17456f687b8d4b80501e
shino omata <shino410stx@gmail.com>
2024-09-22 11:44:09 +0900

    ユーザーコントローラーテスト

--- spec/requests/aquariums_controller_spec.rb
+++ spec/requests/aquariums_controller_spec.rb
@@ -6,23 +6,20 @@ RSpec.describe AquariumsController, type: :request do
 
 
   before(:each) do
-    # 既に存在する場合は再作成せずにユーザーを取得
     @user = User.find_or_create_by(email: 'test@example.com') do |user|
       user.password = 'password123'
       user.username = 'testuser'
     end
-    # ユーザーをサインイン
+    
     sign_in @user
 
-    admin_user = create(:user, admin: true) # 必要に応じてadmin権限を持つユーザーを作成
+    admin_user = create(:user, admin: true) 
     sign_in admin_user
   end
   
   describe 'GET #index' do
     it 'indexアクションが成功すること' do
       get aquariums_path
-      Rails.logger.debug "Response Status: #{response.status}"
-      Rails.logger.debug "Response Body: #{response.body}"
       expect(response).to have_http_status(:success)
       expect(response.body).to include(aquarium.name)
     end
@@ -44,7 +41,7 @@ RSpec.describe AquariumsController, type: :request do
 
   describe 'GET #nearby' do
     it '位置情報が提供されていない場合、リダイレクトされること' do
-      get nearby_aquariums_path # パラメータなしでリクエスト
+      get nearby_aquariums_path 
       expect(response).to redirect_to(aquariums_path)
       expect(flash[:alert]).to eq('位置情報が提供されていません。')
     end
